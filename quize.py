cc8a75a6d1f2dec2e8ee304f971961c6d11505b6
khadija-6 <khadija6.abdi@protonmail.com>
2021-08-27 09:21:23 +0300

    quize projects

---
+++ quize.py
@@ -0,0 +1,27 @@
+import time
+
+print("My name is khadija\nI use pogram with python\nWelcome to use it )
+
+date = input("today it is sunny").capitalize()
+answer = True
+
+if date == str(answer):
+    print("correct")
+else:
+    print("incorrect")
+
+variable =int(input("five take away three"))
+answer = 2
+
+if variable == int answer:
+    print("good")
+else:
+    print("wrog")
+
+book = input("you want milk ")
+answer = "yes"
+
+if book == "yes":
+    print("correct")
+else:
+    print("incorrect")
